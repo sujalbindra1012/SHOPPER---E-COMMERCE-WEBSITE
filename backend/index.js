5b26912d7528d49df42ad25618c8c87272a1e416
sujalbindra1012 <sujalbindra@gmail.com>
2024-08-19 22:58:02 +0530

    e-commerce website

---
+++ backend/index.js
@@ -0,0 +1,231 @@
+const port=4000;
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const jwt=require("jsonwebtoken");
+const multer=require("multer");
+const path=require("path");
+const cors=require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//database connection with mongodb
+mongoose.connect("mongodb+srv://sujalbindra:bindra22sahab@cluster0.jttsfvt.mongodb.net/e-commerce");
+
+//api creation
+
+app.get("/",(req,res)=>{
+    res.send("express app is running...")
+})
+
+//image storage engine 
+
+const storage=multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload =multer({storage:storage})
+
+//creating upload endpoint for images
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//schema for creating products 
+
+const Product=mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async(req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array=products.slice(-1);
+        let last_product=last_product_array[0];
+        id=last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product=new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+  
+//creating api for deleting products 
+
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+//creating api for getting all products 
+app.get('/allproducts',async (req,res)=>{
+    let products= await Product.find({});
+    console.log(" ALL PRODUCTS FETCHED ");
+    res.send(products);
+})
+
+//schema crating for user model 
+const Users=mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+//creating endpoint for  registering the user
+
+app.post('/signup',async(req,res)=>{
+    let check=await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+
+    }
+    let cart={};
+    for(let i=0;i<300;i++){
+        cart[i]=0;
+    }
+    const user=new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token=jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+
+})
+
+
+//creating endpoint for user login 
+app.post('/login', async (req, res) => {
+    try {
+        let user = await Users.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ success: false, errors: "User not found" });
+        }
+
+        const passCompare = req.body.password === user.password;
+        if (!passCompare) {
+            return res.status(400).json({ success: false, errors: "Wrong password" });
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        };
+
+        const token = jwt.sign(data, 'secret_ecom');
+        res.json({ success: true, token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, errors: "Internal server error" });
+    }
+ });
+// // Creating endpoint for new collection data
+// app.get('/newcollections', async (req, res) => {
+//     try {
+//         let products = await Product.find({});
+//         let newcollection = products.slice(1).slice(-8);
+//         console.log("Newcollection Fetched");
+//         res.send(newcollection);
+//     } catch (error) {
+//         console.error("Error fetching new collection:", error);
+//         res.status(500).json({ success: false, errors: "Internal server error" });
+//     }
+// });
+app.use(express.json()); // Add this line to parse JSON bodies
+
+app.post('/addtocart',async(req,res)=>{
+    console.log(req.body);
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("server running on port "+port)
+    }
+    else{
+        console.log("error : "+error)
+    }
+});
---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+const DescriptionBox = () =>{
+    return(
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit pariatur laudantium molestias! Debitis deleniti eaque possimus vero asperiores blanditiis voluptatem odio nostrum repellat tenetur, nobis ipsam dolores quasi saepe? Tempora nesciunt, aut minus id, eum esse repudiandae magnam tenetur, blanditiis officia quisquam placeat assumenda ullam sed eligendi sequi laboriosam reprehenderit!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit totam necessitatibus quam praesentium exercitationem delectus vel vero omnis aspernatur! Aut veritatis consequatur aperiam officiis vitae quia rerum asperiores eius, beatae magni repudiandae, laborum inventore natus quaerat culpa unde. Repellendus molestias id nihil numquam inventore, tenetur obcaecati minus harum odit doloribus.</p>
+                
+            </div>
+        </div>
+
+    )
+}
+
+export default DescriptionBox
